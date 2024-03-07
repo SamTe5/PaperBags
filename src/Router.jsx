152a059c3b90e3aaf94b54737889809f7492082b
SametKiybik <samettemas05@gmail.com>
2024-03-07 09:52:19 +0300

    stack

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,17 +6,38 @@ import { SafeAreaView,Text } from 'react-native';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 const Stack=createNativeStackNavigator()
 const Tab=createBottomTabNavigator()
+
+const HomeStack=()=>{
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name={HomeScreen} component={Home}/>
+    </Stack.Navigator>
+    
+  )
+}
+
+const ProfileStack=()=>{
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name={ProfileScreen} component={Profile}/>
+    </Stack.Navigator>
+    
+  )
+}
+
+
 
 const App=()=> {
   
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Profile" component={Profile}/>
+        <Tab.Screen name="Home" component={HomeStack}/>
+        <Tab.Screen name="Profile" component={ProfileStack}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
