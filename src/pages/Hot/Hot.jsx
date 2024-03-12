379a6b738d7e0b2120d89b24581a1f9a797533d9
SametKiybik <samettemas05@gmail.com>
2024-03-12 08:13:46 +0300

    Hot page

---
+++ src/pages/Hot/Hot.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView,Text } from "react-native";
+
+const Hot =({navigation})=>{
+    return(
+        <SafeAreaView>
+            <Text>
+                Hot
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default Hot;
